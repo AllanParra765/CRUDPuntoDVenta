b14f045c5271067dfea881f3823628d013e75310
AllanParra765 <96440100+AllanParra765@users.noreply.github.com>
2023-05-22 13:12:05 -0600

    Separación de header y footer
    Se separa encabezado y pie de pagina index para dejar un código limpio y sostenible y evitar afectar su mantenimiento y se utiliza el import para llamar al archivo que contiene el código para esa parte del código

---
+++ Vistas/partials/footerindex.php
@@ -0,0 +1,5 @@
+<!-- Asegúrate de incluir las dependencias de Bootstrap y Font Awesome en tu proyecto -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</html>
--- index.php
+++ index.php
@@ -1,29 +1,7 @@
 <?php
-// Establecer el tiempo límite de la sesión en segundos (por ejemplo, 30 minutos)
-$tiempoLimite =90 ; //1800 30 minutos
-session_start();
-
-// Verificar si hay un mensaje de alerta en la sesión
-if (isset($_SESSION['alert'])) {
-    // Obtener los detalles de la alerta
-    $alertType = $_SESSION['alert']['type'];
-    $alertMessage = $_SESSION['alert']['message'];
-
-    // Mostrar la alerta
-    echo '<div class="alert alert-' . $alertType . ' alert-dismissible fade show" role="alert">';
-    echo $alertMessage;
-    echo '<button type="button" class="close" data-dismiss="alert" aria-label="Close">';
-    echo '<span aria-hidden="true">&times;</span>';
-    echo '</button>';
-    echo '</div>';
-
-    // Limpiar el mensaje de alerta de la sesión
-    unset($_SESSION['alert']);
-}
-// Establecer el nuevo tiempo de expiración de la sesión
-$_SESSION['expire'] = time() + $tiempoLimite;
-
+include "Controlador/sesiones/headerSesion.php";
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -53,9 +31,6 @@ $_SESSION['expire'] = time() + $tiempoLimite;
         </form>
     </div>
 </body>
-<!-- Asegúrate de incluir las dependencias de Bootstrap y Font Awesome en tu proyecto -->
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-</html>
-
+<?php
+include "Vistas/patials/footerindex.php";
+?>
